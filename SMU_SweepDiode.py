c7cc3f17adae676971db450067fc8af80aa4649a
unknown <jairuszac@gmail.com>
2024-11-18 12:48:02 +0800

    Refactor file exports

--- SMU_SweepDiode.py
+++ SMU_SweepDiode.py
@@ -7,10 +7,10 @@ instruments = rm.list_resources()
 print(instruments)
 
 # Follow your address
-smu = rm.open_resource(instruments[2])
+smu = rm.open_resource('TCPIP0::192.168.200.5::inst0::INSTR')
 
 # USE Type switch to Host mode, else SMU cant export CSV files into Drive
-# smu.write("SYST:COMM:USB:TYPE HOST")
+smu.write("SYST:COMM:USB:TYPE HOST")
 
 # Make SMU into Current mode
 smu.write("FUNC:MODE CURR")
@@ -25,9 +25,10 @@ smu.write("SWE:STAR 0.001")
 smu.write("SWE:STOP 0.05")
 # smu.write("SWE:STEP 0.001")
 smu.write("SWE:POIN 100")
-smu.write("SOURce1:SWEep:TRIG:STArt:SOURCE IMMediate")
+smu.write("SWEep:TRIG:STArt:SOURCE IMMediate")
 smu.write("OUTPUT 1")
-smu.write("SOURce:SWEep:INITiate")
+
+smu.write("SOURCE:SWEep:INIT")
 
 #Setting on Graph
 smu.write("SWE:GRAP:CONF:CURV:NUMB 1")
@@ -35,11 +36,12 @@ smu.write("SWE:GRAP:VOLT:SCAL LINear")
 smu.write("SWE:GRAP:CURR:SCAL LINear")
 smu.write("SWE:GRAP:VOLT:INVE 0")
 smu.write("SWE:GRAP:CURR:INVE 0")
+
 # Export to USB [Insert your filename here], hint you can create a loop to roll the name with cycle time.
-print(smu.write("SWE:GRAP:VIEW:CURV:DATA? 10"))
+time.sleep(15) # should be greater than the total estimated sweep time. Acts as buffer time.
 
-smu.write("SWE:DATA:EXP IT2806_sweep_data00005") # error thrown here
+filename = "myfile"
+status = smu.query("SWE:STAT?")
 
-#Mini Branch looks good, finger licking good.
-# gago naman
-# dota tayo
+if "IDLE" in status:
+    smu.write(f"SWE:DATA:EXP \"{filename}\"")
