bcd3e91a7571dcf8b5105c6b3f73b1df2685331d
unknown <sohyingju0327@hotmail.com>
2024-12-02 14:41:07 +0800

    added new function to pycisa_scan

---
+++ SMU_SweepVoltage.py
@@ -0,0 +1,48 @@
+import pyvisa
+import time
+
+rm = pyvisa.ResourceManager()
+instruments = rm.list_resources()
+
+print(instruments)
+
+# Follow your address
+# smu = rm.open_resource('TCPIP0::192.168.200.5::inst0::INSTR')
+smu = rm.open_resource(rm.list_resources[i])
+
+# USE Type switch to Host mode, else SMU cant export CSV files into Drive
+smu.write("SYST:COMM:USB:TYPE HOST")
+
+# Make SMU into Voltage mode
+smu.write("FUNC:MODE VOLT")
+
+#Sweep Linear Mode
+# smu.write("SOURCE:SWEep:LINear")
+smu.write("SWE:SPAC LIN")
+smu.write("SWE:STA SINGLE")
+
+# Sweep Parameter, start ~ end, step size, no of points
+smu.write("SWE:STAR -5") 
+smu.write("SWE:STOP 5")
+smu.write("SWE:STEP 0.1")
+# smu.write("SWE:POIN 100")
+smu.write("SWEep:TRIG:STArt:SOURCE IMMediate")
+smu.write("OUTPUT 1")
+
+smu.write("SOURCE:SWEep:INIT")
+
+#Setting on Graph
+smu.write("SWE:GRAP:CONF:CURV:NUMB 1")
+smu.write("SWE:GRAP:VOLT:SCAL LINear")
+smu.write("SWE:GRAP:CURR:SCAL LINear")
+smu.write("SWE:GRAP:VOLT:INVE 0")
+smu.write("SWE:GRAP:CURR:INVE 0")
+
+# Export to USB [Insert your filename here], hint you can create a loop to roll the name with cycle time.
+time.sleep(15) # should be greater than the total estimated sweep time. Acts as buffer time.
+
+filename = "myfile"
+status = smu.query("SWE:STAT?")
+
+if "IDLE" in status:
+    smu.write(f"SWE:DATA:EXP \"{filename}\"")
--- pyvisa_scan.py
+++ pyvisa_scan.py
@@ -4,9 +4,14 @@ rm = pyvisa.ResourceManager()
 resources = rm.list_resources()
 
 if resources:
-    print("Connected VISA resources:")
     for resource in resources:
-        print(resource)
+        # print("Resource:" , resource)
+        equipment = rm.open_resource(resource)
+        # print("Equipment:" , equipment)
+        # print("Connected VISA resources:" + resource + ". IDN:" + equipment.write("IDN"))
+        IDN = equipment.write("*IDN?")
+        print(f"Connected VISA resources: {resource} IDN: {IDN}")
+        # equipment.close()
 else:
     print("No VISA resources found.")
 
